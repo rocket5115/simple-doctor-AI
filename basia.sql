4c9fe14d7a5a0b2b735e46729b59d284434822db
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-05-08 16:59:11 +0200

    Create basia.sql

---
+++ basia.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `basia`;
+CREATE TABLE IF NOT EXISTS `basia` (
+  `id` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `hp` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
