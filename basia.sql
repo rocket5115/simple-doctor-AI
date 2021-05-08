9c1ea888455458660c2f4ceab0ad5e26cd6930c7
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-05-08 16:59:31 +0200

    Update basia.sql

--- basia.sql
+++ basia.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `basia` (
   `id` varchar(50) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `ip` varchar(50) DEFAULT NULL,
-  `hp` varchar(255) DEFAULT NULL
+  `token` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
