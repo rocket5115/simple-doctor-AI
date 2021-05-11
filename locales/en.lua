a2d110914eceef5fad15abe94a148888a0ffeea1
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-05-11 16:22:32 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+    ['start_message'] = 'Doctor is helping you!',
+    ['wait_message'] = 'Doctor will help you in 15 seconds!',
+    ['wait_message2'] = 'Doctor will help you in 10 seconds!',
+    ['wait_message3'] = 'Doctor will help you in 5 seconds!',
+    ['success_message'] = 'Doctor healed you!',
+
+    ['EMS_error'] = 'Too many EMS in service!',
+
+    ['text_content'] = '~b~Press ~r~[E] ~b~to seek help from doctor'
+}
---
+++ locales/pl.lua
@@ -0,0 +1,11 @@
+Locales['pl'] = {
+    ['start_message'] = 'Doktor się tobą zajmuje!',
+    ['wait_message'] = 'Doktor ci pomaga. Poczekaj jeszcze 15 sekund!',
+    ['wait_message2'] = 'Doktor ci pomaga. Poczekaj jeszcze 10 sekund!',
+    ['wait_message3'] = 'Doktor ci pomaga. Poczekaj jeszcze 5 sekund!',
+    ['success_message'] = 'Doktor ci pomógł!',
+
+    ['EMS_error'] = 'Zbyt dużo dostępnych EMS na służbie!',
+
+    ['text_content'] = '~b~Naciśnij ~r~[E] ~b~ aby otrzymać pomoc lekarską'
+}
