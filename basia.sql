3c195e759555580d4b2a392379d235623c25ddf1
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-05-11 16:22:43 +0200

    Delete basia.sql

--- basia.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `basia`;
-CREATE TABLE IF NOT EXISTS `basia` (
-  `id` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `ip` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
